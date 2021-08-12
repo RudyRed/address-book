058899fa9a3c85b6ca69df2a78276aa7ed2ab342
RudyRed <will.gross@nearform.com>
2021-08-12 15:48:49 -0400

    add babel plugin for async/await

--- babel.config.js
+++ babel.config.js
@@ -11,6 +11,12 @@ module.exports = {
     '@babel/plugin-proposal-object-rest-spread',
     '@babel/plugin-proposal-optional-chaining',
     '@babel/plugin-proposal-nullish-coalescing-operator',
+    [
+      '@babel/plugin-transform-runtime',
+      {
+        regenerator: true,
+      },
+    ],
   ],
   env: {
     test: {
