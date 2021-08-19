f6477751cf2d85e72701b51143b6c4be9a86f50b
RudyRed <will.gross@nearform.com>
2021-08-19 00:59:29 -0400

    feat: add `getUser` fetch to `Details` page

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -1,19 +1,26 @@
+import { useEffect, useState } from 'react';
 import { Helmet } from 'react-helmet-async';
 
 import Layout from '../components/Layout/Layout';
 import PersonCard from '../components/PersonCard';
+import { getUser } from '../services';
 
 const Details = ({ match }) => {
-  console.log(match?.params.personId, 'props');
+  const [user, setUser] = useState();
+
+  useEffect(() => {
+    const getAndSetUser = async () => {
+      setUser(await getUser(match?.params.personId));
+    };
+
+    getAndSetUser();
+  }, []);
+
   return (
     <div>
       <Helmet title="Details" />
       <Layout title="Details">
-        <PersonCard
-          firstName="William"
-          lastName="Gross"
-          phoneNumber="647-230-9876"
-        />
+        <PersonCard {...user} />
       </Layout>
     </div>
   );
--- src/pages/Details.test.js
+++ src/pages/Details.test.js
@@ -1,10 +1,21 @@
 import { render, screen } from '@testing-library/react';
 
 import Details from './Details';
+import mockedGetUsers from './__mocks__/Home-getUsers.mock';
 
 jest.mock('react-helmet-async', () => ({
   Helmet: () => <div />,
 }));
+
+global.fetch = jest.fn(() =>
+  Promise.resolve({
+    json: () => Promise.resolve(mockedGetUsers),
+  }),
+);
+
+beforeEach(() => {
+  fetch.mockClear();
+});
 
 const setup = (overrides) => {
   const props = {
@@ -17,9 +28,15 @@ const setup = (overrides) => {
 };
 
 describe('Details', () => {
-  it('Should render details', async () => {
+  it('Should render Details page', async () => {
     setup();
 
     expect(await screen.findByText('Details')).toBeInTheDocument();
+  });
+
+  it('Should fetch and render user via PeopleCard', async () => {
+    const { findByText } = setup();
+
+    expect(await findByText('(64) 6431-7116')).toBeInTheDocument();
   });
 });
